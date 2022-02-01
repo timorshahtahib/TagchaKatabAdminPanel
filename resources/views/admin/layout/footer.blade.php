10198cf902e40cae3f672c7d127fa4a2d77a31c9
Erfan.computer <TimorshahTahib@gmail.com>
2022-02-01 22:29:09 +0330

    NewFeture

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="footer">
     <div class="container-fluid">
         <nav class="pull-right">
-
         </nav>
         <p class="copyright pull-left">
             1400 &copy; راست‌به‌چپ شده توسط <a href="https://poraanict.com" target="_blank">تیمورشاه</a>
